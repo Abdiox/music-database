cef29620bea8bcca32a9387ccd248cd6f3215c30
Abdiox <abdirahmanroble2610@gmail.com>
2023-09-21 15:15:12 +0200

    yoink

---
+++ database.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2/promise"; // using mysql2 - installed npm library
+import "dotenv/config";
+import fs from "fs/promises";
+
+const connection = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+  multipleStatements: true,
+};
+
+if (process.env.MYSQL_CERT) {
+  connection.ssl = { ca: await fs.readFile("DigiCertGlobalRootCA.crt.pem") };
+}
+
+const dbConnection = await mysql.createConnection(connection);
+
+export default dbConnection;
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fs from "fs/promises";
+import cors from "cors";
+import dbConnection from "./database.js";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("Node.js Users REST API 🎉");
+});
+
+app.listen(port, () => {
+  console.log(`serveren kører på http://localhost:3000`);
+});
+
+app.get("/", async (request, response) => {
+  response.send("Hello my brother");
+});
+
+app.get("/artist", async (request, response) => {
+  const query = "SELECT * FROM music_db1 ORDER BY name;";
+  dbConnection.query(query, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+      response.json({ message: error });
+    } else {
+      response.json(results);
+    }
+  });
+});
