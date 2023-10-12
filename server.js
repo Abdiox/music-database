9d82edab40aa15e79614c21b31f361c9980587d1
Abdiox <abdirahmanroble2610@gmail.com>
2023-10-12 13:57:20 +0200

    yup

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ import artistsRouter from "./routes/artists.js";
 import albumsRouter from "./routes/albums.js";
 import songsRouter from "./routes/songs.js";
 import searchRouter from "./routes/searchRouter.js";
+import Debug from "debug";
 
 const app = express();
 const port = process.env.PORT || 3333;
+const debug = Debug("app:startup");
+
+debug("Something Happened");
 
 app.use(express.json());
 app.use(cors());
